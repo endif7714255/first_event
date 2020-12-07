3df326525c68b1f5be2264797e1d97a609fa09d7
王娜 <2433621680@qq.com>
2020-12-07 19:14:43 +0800

    完成了后台主页的基本功能

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,10 +1,10 @@
 $(function () {
-  // 点击去注册链接事件
+  // 点击注册链接事件
   $("#link_reg").on("click", function () {
     $(".login-box").hide();
     $(".reg-box").show();
   });
-  // 点击去登陆链接事件
+  // 点击登陆链接事件
   $("#link_login").on("click", function () {
     $(".login-box").show();
     $(".reg-box").hide();
@@ -18,10 +18,6 @@ $(function () {
     pwd: [/^[\S]{6,12}$/, "密码必须6到12位，且不能出现空格"],
     // 制定密码一致规则
     repwd: function (value) {
-      // 通过形参拿到的是确认密码框中的内容
-      // 还需要拿到密码框中的内容
-      // 然后进行一次等于的判断
-      // 如果判断失败,则return一个提示消息即可
       var pwd = $(".reg-box [name=password]").val();
       if (pwd !== value) {
         return "两次密码不一致";
@@ -55,7 +51,7 @@ $(function () {
           return layer.msg("登陆失败！");
         }
         layer.msg("登录成功！");
-        console.log(res.spoken);
+        // 将token值存储到本地并跳转到后台主页
         localStorage.setItem("token", res.token);
         location.href = "index.html";
       }
